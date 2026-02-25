4781e5ee7cb62f8eaaec9b3921d3dcf035eb568b
hongminhle0399 <hongminhle0399@gmail.com>
2026-02-25 18:03:05 +0700

    feat: Refactor and add selected navigation item

--- src/layouts/MainLayout/components/TheHeader.tsx
+++ src/layouts/MainLayout/components/TheHeader.tsx
@@ -1,48 +1,92 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import {
-  UserGroupIcon,
-  ShoppingBagIcon,
-  MoonIcon,
-  SunIcon,
-} from "@heroicons/react/20/solid";
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Image,
+} from "@heroui/react";
+
+import { MoonIcon, SunIcon } from "@heroicons/react/20/solid";
 import { toggleDarkMode, useAppSettingsStore } from "@/store/appSettings.store";
-import { Button } from "@heroui/react";
 import { useMemo } from "react";
 
-const HEADER_LINKS = [
+const NAVIGATION_ITEMS = [
   {
     to: "customers",
     text: "Customers",
-    icon: <UserGroupIcon className="size-6" />,
   },
-  { to: "orders", text: "Orders", icon: <ShoppingBagIcon className="size-6" /> },
+  {
+    to: "orders",
+    text: "Orders",
+  },
 ];
 
 const TheHeader = () => {
   const isDarkMode = useAppSettingsStore((state) => state.isDarkMode);
+  const { pathname } = useLocation();
 
-  const darkModeIcon = useMemo(() => isDarkMode ? <SunIcon className="size-6" /> : <MoonIcon className="size-6" />, [isDarkMode])
+  const darkModeIcon = useMemo(() => {
+    const Icon = isDarkMode ? SunIcon : MoonIcon;
+    return (
+      <Icon className="size-6 flex h-5 w-5 items-center justify-center text-slate-500" />
+    );
+  }, [isDarkMode]);
 
   return (
-    <header className="flex items-center justify-between gap-x-4 p-4 border-b-3 rounded-b-lg">
-      <div className="flex gap-x-4">
-        {HEADER_LINKS.map((link) => {
-          const { to, text, icon } = link;
-
-          return (
-            <NavLink key={to} to={to}>
-              <Button color="default" className="flex gap-x-2" variant="light">
-                {icon}
-                <p className="text-lg">{text}</p>
-              </Button>
-            </NavLink>
-          );
-        })}
-      </div>
-      <Button isIconOnly onPress={toggleDarkMode} variant="light">
-        {darkModeIcon}
-      </Button>
-    </header>
+    <Navbar
+      isBordered
+      isBlurred
+      // className="justify-center max-w-350 w-full"
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-primary",
+        ],
+      }}
+    >
+      <NavbarContent className="flex">
+        <NavbarBrand className="basis-auto grow-0 mr-4">
+          <Image
+            src="https://occupass.com/assets/occupass-mpO8hpII.svg"
+            alt="Occupass Logo"
+            className="size-auto"
+            fetchPriority="high"
+          />
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-5">
+          {NAVIGATION_ITEMS.map((nav) => {
+            const isActive = pathname.includes(nav.to);
+            return (
+              <NavbarItem key={nav.to} isActive={isActive}>
+                <Link as={NavLink} color="foreground" to={nav.to}>
+                  {nav.text}
+                </Link>
+              </NavbarItem>
+            );
+          })}
+        </NavbarContent>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Button isIconOnly onPress={toggleDarkMode} variant="light">
+            {darkModeIcon}
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
 };
 
