9776a5bcfd6da610b996e0809d786a858772c30e
hongminhle0399 <hongminhle0399@gmail.com>
2026-03-08 16:21:47 +0700

    fix: Setup default page for routing

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,31 +1,35 @@
-import { AppErrorBoundaries } from "@/AppErrorBoundaries";
 import { MainLayout } from "@/layouts";
-import { CustomersPage, NotFoundPage, OrdersPage, CustomerProfilePage, OrderDetailsPage } from "@/pages";
+import { CustomerProfilePage, CustomersPage, NotFoundPage, OrderDetailsPage, OrdersPage } from "@/pages";
 import { DefaultError } from "@/shared/ui/DefaultError";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   {
     element: <MainLayout />,
+    path: '/',
     errorElement: <DefaultError />,
     children: [
       {
-        path: "/orders",
+        index: true,
+        element: <Navigate to="/orders" replace />,
+      },
+      {
+        path: "orders",
         element: <OrdersPage />,
         handle: { pageName: "Orders" },
       },
       {
-        path: "/orders/:id",
+        path: "orders/:id",
         element: <OrderDetailsPage />,
         handle: { pageName: "Order Detail" },
       },
       {
-        path: "/customers",
+        path: "customers",
         element: <CustomersPage />,
         handle: { pageName: "Customers" },
       },
       {
-        path: "/customers/:id",
+        path: "customers/:id",
         element: <CustomerProfilePage />,
         handle: { pageName: "Customer Detail" },
       },
